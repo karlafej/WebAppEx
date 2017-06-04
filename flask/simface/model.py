411f2fecc79fbd5ba381b21738b09f78daa66aad
Karla Fejfarova <karlafej@gmail.com>
2017-06-04 13:11:35 +0200

    cycle the results

--- flask/simface/model.py
+++ flask/simface/model.py
@@ -1,9 +1,17 @@
-from wtforms import Form, StringField, RadioField, validators, SubmitField
+from flask_wtf import FlaskForm
+from wtforms import  StringField, RadioField, validators, SubmitField
 
-class InputForm(Form):
+
+class InputForm(FlaskForm):
     imgurl = StringField('Photo URL: ', [validators.InputRequired()])
+
+class ImageSet(FlaskForm):
     imageset = RadioField('Find your lookalike among: ', choices=[
         ('science', 'Scientists'),
         ('movie', 'Actors and Actresses'),
         ('porn', 'Pornstars')],
         default='movie')
+
+class Cycle(FlaskForm):
+    cycle = SubmitField("Don't like it? Try again!")
+        
--- flask/simface/view.py
+++ flask/simface/view.py
@@ -1,4 +1,4 @@
-from model import InputForm
+from model import InputForm, ImageSet, Cycle
 from flask import Flask, render_template, request, session
 import simface
 
@@ -7,20 +7,33 @@ app.secret_key = 'sosecretsolongblablabla'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = InputForm(request.form)
+    form1 = InputForm(request.form, prefix="form1")
+    form2 = ImageSet(request.form, prefix="form2")
+    form3 = Cycle(request.form, prefix="form3")
     results = dict()
     
-    if form.imgurl.data:
-        session['face_id'] = simface.get_face_id(form.imgurl.data)
+    if form1.validate_on_submit() and form1.imgurl.data:
+        session['face_id'] = simface.get_face_id(form1.imgurl.data)
+        session['k'] = 0
+        
     if 'face_id' in session:
-        if form.imageset.data: 
-            idx = form.imageset.data
-            list_id = simface.imageset[idx]
-            similars = simface.face_similar(session['face_id'], list_id)
-            results = simface.get_info(idx, similars[0]['persistedFaceId'])
-            results['confidence'] = similars[0]['confidence']
-     
-    return render_template('view.html', form=form, results = results)
+        if form2.validate_on_submit() and form2.imageset.data: 
+            session['k'] = 0
+            session['idx'] = form2.imageset.data
+            list_id = simface.imageset[session['idx']]
+            session['similars'] = simface.face_similar(session['face_id'], list_id)
+            results = simface.get_info(session['idx'], session['similars'][0]['persistedFaceId'])
+            results['confidence'] = session['similars'][0]['confidence']
+    
+    if form3.validate_on_submit() and form3.cycle.data:
+        if session['k'] < (len(session['similars'])-1):
+            session['k'] = session['k']+1
+        else:
+            session['k'] = 0
+        results = simface.get_info(session['idx'], session['similars'][session['k']]['persistedFaceId'])
+        results['confidence'] = session['similars'][session['k']]['confidence']
+  
+    return render_template('view.html', form1=form1, form2 = form2, form3 = form3, results = results)
 
 if __name__ == '__main__':
     app.run(debug=True)
